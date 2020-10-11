e3b6f8a5f5069ed9ba4366083d6a6c9ead7077f7
OlliHemminki <olli.hemminki@gmail.com>
2020-10-11 10:45:11 +0300

    commit 2

--- src/laskuri/Laskuri.java
+++ src/laskuri/Laskuri.java
@@ -18,6 +18,9 @@ public class Laskuri{
   	public void lisaa(){
     		this.tapahtumia = this.tapahtumia + 1; 
   	}
+        public void lisaa(int lisays){
+    		this.tapahtumia = this.tapahtumia + lisays; 
+  	}
   	public int lukema(){
     		return this.tapahtumia;
   	}
@@ -25,7 +28,7 @@ public class Laskuri{
     		this.tapahtumia = 0;
   	}
 	public String toString(){
-		return "Laskurin lukema on " + this.tapahtumia;
+		return "Lukema: " + this.tapahtumia;
 	}
 
 }
